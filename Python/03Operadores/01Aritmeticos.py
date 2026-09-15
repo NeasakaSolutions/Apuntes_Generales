50c39944a3c34fe7ae1eb07e461fc2bd357ed471
Mai <satou.matsuzaka.3443@gmail.com>
2026-09-15 13:14:45 +0000

    Se empezaron con los operadores aritmeticos

---
+++ Python/03Operadores/01Aritmeticos.py
@@ -0,0 +1,21 @@
+# Variables:
+num_uno = 5
+num_dos = 3
+texto_uno = 'ijole'
+texto_dos = 'el diablo'
+
+# Operaciones
+suma = num_uno + num_dos
+resta = num_uno - num_dos
+producto = num_uno * num_dos
+potencia = num_uno ** num_dos
+division = num_uno / num_dos
+suma_strings = texto_uno + texto_dos
+
+# Impresion de resultados:
+print(f'La suma de {num_uno} y { num_dos} es: {suma}')
+print(f'La resta de {num_uno} y { num_dos} es: {resta}')
+print(f'El producto de {num_uno} y { num_dos} es: {producto}')
+print(f'La division de {num_uno} y { num_dos} es: {division}')
+print(f'La potencia de {num_uno} con potencia { num_dos} es: {potencia}')
+print(f'Suma de cadenas de texto: {suma_strings}') # No se pueden restar cadenas de texto
